4e45fd6831ab7c88a59f9f193cb0dfa77958758a
Mingxiang.Zhang <ming.zhang@gree.net>
2013-05-31 17:33:24 +0900

    seperate ipvs_real_server from keepalived::real_server which is a exported resources

---
+++ manifests/ipvs_real_server.pp
@@ -0,0 +1,15 @@
+define keepalived::ipvs_real_server($virtual_server_ip) {
+  $octet_id = values_at(split($virtual_server_ip, '[.]'), 3)
+  $service_name = "ipvs_real_server_${octet_id}"
+
+  file { "/etc/init.d/${service_name}" :
+    ensure  => present,
+    content => template('keepalived/debian/ipvs_real_server.init.erb'),
+  }
+
+  service { $service_name :
+    enable => true,
+    ensure => false,
+    require => File["/etc/init.d/${service_name}"],
+  }
+}
--- manifests/real_server.pp
+++ manifests/real_server.pp
@@ -33,17 +33,4 @@ define keepalived::real_server(
       order   => 50;
   }
 
-  $octet_id = values_at(split($virtual_server_ip, '[.]'), 3)
-  $service_name = "ipvs_real_server_${octet_id}"
-  file { "/etc/init.d/${service_name}" :
-    ensure  => present,
-    content => template('keepalived/debian/ipvs_real_server.init.erb'),
-  }
-
-  service { $service_name :
-    enable => true,
-    ensure => false,
-    require => File["/etc/init.d/${service_name}"],
-  }
-
 }
